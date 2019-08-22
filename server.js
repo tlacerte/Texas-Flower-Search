f98a2ed3194c82a04e1948eff4fcaa9c6508820f
Taylor Lacerte <taylorlacerte@Taylors-MacBook-Pro.local>
2019-08-22 14:39:27 -0500

    add readme

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var flowersRouter = require('./routes/flowers');
 var apiRouter = require('./routes/api');
+var cors = require('cors');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/flowers', flowersRouter);
 app.use('/api', apiRouter);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
