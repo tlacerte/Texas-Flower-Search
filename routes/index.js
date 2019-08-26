251a4430d3d05dc1a5b99deae7abcd115beadc75
Taylor Lacerte <taylorlacerte@Taylors-MacBook-Pro.local>
2019-08-26 14:32:34 -0500

    index redirect

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/flowers/index');
+  res.redirect('/flowers');
 });
 
 router.get('/auth/google', passport.authenticate(
