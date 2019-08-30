3c5563bd8226f88caa67c36911df9d38e1bd87ef
Taylor Lacerte <taylorlacerte@Taylors-MacBook-Pro.local>
2019-08-30 12:49:03 -0500

    function comments

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,6 +6,7 @@ module.exports = {
   update
 };
 
+//add a review
 function create(req, res) {
   Flower.findById(req.params.id, function(err, flower) {
     flower.reviews.push(req.body);
@@ -15,6 +16,7 @@ function create(req, res) {
   });
 }
 
+//edit button on show page to get to edit page
 async function edit(req, res) {
     let flower = await Flower.findOne({_id: req.params.id})
     res.render('flowers/edit-review', {
@@ -24,6 +26,7 @@ async function edit(req, res) {
     });
 }
 
+//save the new review and go back to show page
 async function update(req, res){
     let flower = await Flower.findOne({_id: req.params.id});
     flower.reviews[req.params.idx].content = req.body.content;
